e923533314a22e495562aed111deb22759696125
TatsuakiIto <gratachu9@gmail.com>
2023-07-26 21:56:01 +0900

    Add: CardTitle handling

--- src/comopnents/task/TaskCardTitle.jsx
+++ src/comopnents/task/TaskCardTitle.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TaskCardTitle = () => {
+  const [isClick, setIsClick] = useState(false)
+  const [inputCardTitle, setInputCardTitle] = useState('Today')
+
+  const handleClick = () => {
+    setIsClick(true)
+  }
+
+  const handleChange = (e) => {
+    setInputCardTitle(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    // Enterを押したらページが更新されるといるデフォルトの操作を防ぐ
+    e.preventDefault()
+    setIsClick(false)
+  }
+
   return (
-    <div>
-      
+    <div onClick={handleClick}>
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input type='text' onChange={handleChange}/>
+        </form>
+        ) : (
+        <h3>{inputCardTitle}</h3>
+        )}
     </div>
   )
 }
