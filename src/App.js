1b2e0069ceba256a62272159c401f04299b3f7df
TatsuakiIto <gratachu9@gmail.com>
2023-07-26 21:17:47 +0900

    Add: taskcards, taskcard component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./comopnents/header/Header";
+import TaskCards from "./comopnents/task/TaskCards";
 
 function App() {
   return <div className="app">
     <Header />
+    <TaskCards />
   </div>
 }
 
