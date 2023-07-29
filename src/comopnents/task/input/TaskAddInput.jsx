369ef3cf270b1d2b8563047eed6944cdcf5ee2ce
TatsuakiIto <gratachu9@gmail.com>
2023-07-29 22:50:03 +0900

    taskAddInputのスタイルと基本的な挙動の作成

--- src/comopnents/task/input/TaskAddInput.jsx
+++ src/comopnents/task/input/TaskAddInput.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const TaskAddInput = () => {
+  const handleSubmit = (e) => {
+    console.log(e)
+    e.preventDefault()
+  }
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='add a task' className='taskAddInput'/>
+      </form>
       
     </div>
   )
