576c5da3d7765944d81656706a62d6b080f2df93
TatsuakiIto <gratachu9@gmail.com>
2023-09-02 21:21:56 +0900

    feat: add taskcard delete button style

--- src/comopnents/task/button/TaskCardDeleteButton.jsx
+++ src/comopnents/task/button/TaskCardDeleteButton.jsx
@@ -1,9 +1,12 @@
+
 import React from 'react'
 
 const TaskCardDeleteButton = () => {
   return (
     <div>
-      
+      <button className='taskCardDeleteButton'>
+      <i class="fa-regular fa-circle-xmark"></i>
+      </button>      
     </div>
   )
 }
